ca2b571b72b322b2703b2bc2ce7a8940444a3636
WickerPrison <cobylax17@gmail.com>
2024-04-01 11:05:11 -0600

    set up basics of ui

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const express = require('express');
 //const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
@@ -8,6 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 //app.use(session(sess));
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
