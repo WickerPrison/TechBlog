cc9bcca756f7eaaf8e55b29c65409e9dd1c5b391
WickerPrison <cobylax17@gmail.com>
2024-04-02 09:05:59 -0600

    can view comments

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -8,6 +8,17 @@ router.get('/', async (req, res) => {
     res.json(posts);
 });
 
+router.get('/:id', async (req, res) => {
+    const posts = await Posts.findOne(
+        {
+            include: {model: Comments},
+            where: {id: req.params.id}
+        }).catch((err) =>{
+        res.json(err);
+    });
+    res.json(posts);
+});
+
 router.post('/', async (req, res) => {
     const newPost = await Posts.create(req.body).catch((err) => {
         res.status(400).json(err);
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,11 +8,34 @@ router.get('/', async (req,res) => {
     });
 
     const posts = postsData.map((post) => post.get({plain: true}));
-    res.render('post', {posts,
+    res.render('posts', {posts,
+    logged_in: req.session.logged_in});
+});
+
+router.get('/posts/:id', withAuth, async (req,res) => {
+    const postData = await Posts.findOne({
+        where: {id: req.params.id}
+    }).catch((err) => {
+        res.json(err);
+    });
+
+    const post = postData.get({plain: true});
+
+    const commentsData = await Comments.findAll(
+        {
+            where: {post_id: req.params.id},
+            include: {model: Posts}
+        }).catch((err) =>{
+        res.json(err);
+    });
+    const comments = commentsData.map((comment) => comment.get({plain: true}));
+
+    res.render('post', {post, comments,
     logged_in: req.session.logged_in});
 });
 
 router.get('/login', async (req,res) => {
+    console.log("test");
     res.render('login');
 });
 
